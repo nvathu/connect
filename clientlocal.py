dbb728e146ad236942323b1a1b75c246ec8bc1d6
Thư <1759038@itec.hcmus.edu.vn>
2021-02-04 11:13:32 +0700

    workincom

--- clientlocal.py
+++ clientlocal.py
@@ -1,11 +1,9 @@
 #raspberry run
 import socketio
-import cups
 
 queue = '/printer-queue'
 sio = socketio.Client()
-conn = cups.Connection()
-printers = conn.getPrinters()
+
 # def send_sensor_reading():
     # sio.emit('test', {'data': 'This is test data'}, namespace= queue)
     # sio.emit('joinClientQueue', namespace= queue)
@@ -15,7 +13,7 @@ printers = conn.getPrinters()
 def connect(namespace=queue):
     # sio.start_background_task(send_sensor_reading)
     print('connection established')
-    sio.emit('test', {'data': 'This is a test data'}, namespace= queue)
+    # sio.emit('test', {'data': 'This is a test data'}, namespace= queue)
     print('my sid is', sio.sid)
     
     # sio.emit('joinClientQueue', namespace= '/printer-queue')
@@ -28,10 +26,6 @@ sio.connect('http://localhost:5000', headers={'tenantId':'1'},namespaces = ['/pr
 
 
 print("abc")
-for printer in printers:
-    sio.emit('test', printer, namespace= queue)
+sio.emit('test', {'data': 'This is a test data'}, namespace= queue)
 
 
-printer_name=printers.keys()[0]
-fileName = "hello.txt"
-conn.printFile(printer_name, fileName, " ", {})
